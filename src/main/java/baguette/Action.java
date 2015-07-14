ac8455d22da6f6db0e379a28b9c6764c6e3600dc
goujonpa <goujonpa@gmail.com>
2015-07-14 22:31:22 +0200

    Translation

--- src/main/java/baguette/Action.java
+++ src/main/java/baguette/Action.java
@@ -3,72 +3,72 @@ package baguette;
 import robocode.AdvancedRobot;
 
 public class Action {
-    private int        tipo;
-    private double     parametro;
-    private int        prioridad;
+    private int        type;
+    private double     parameter;
+    private int        priority;
 
-    private AdvancedRobot robot;   // Referncia al robot que ejecutara la accion
+    private AdvancedRobot robot;   // Reference to the bot who will execute the action
 
-    public static final int AVANZAR=1;
-    public static final int RETROCEDER=2;
+    public static final int AHEAD=1;
+    public static final int BACK=2;
     public static final int STOP=3;
-    public static final int DISPARAR=4;
-    public static final int GIRAR_TANQUE_DER=5;
-    public static final int GIRAR_TANQUE_IZQ=6;
-    public static final int GIRAR_RADAR_DER=7;
-    public static final int GIRAR_RADAR_IZQ=8;
-    public static final int GIRAR_CANON_DER=9;
-    public static final int GIRAR_CANON_IZQ=10;
+    public static final int SHOOT=4;
+    public static final int TANK_RIGHT=5;
+    public static final int TANK_LEFT=6;
+    public static final int RADAR_RIGHT=7;
+    public static final int RADAR_LEFT=8;
+    public static final int GUN_RIGHT=9;
+    public static final int GUN_LEFT=10;
 
 
     public Action() {
     }
 
-    public Action(int tipo, double parametro, int prioridad) {
-        this.tipo = tipo;
-        this.parametro = parametro;
-        this.prioridad = prioridad;
+    public Action(int type, double parameter, int priority) {
+        this.type = type;
+        this.parameter = parameter;
+        this.priority = priority;
     }
 
-    public double getParametro() {
-        return parametro;
+    public double getParameter() {
+        return parameter;
     }
 
-    public void setParametro(double parametro) {
-        this.parametro = parametro; 
+    public void setParameter(double parameter) {
+        this.parameter = parameter; 
     }
 
-    public int getTipo() {
-        return tipo;
+    public int getType() {
+        return type;
     }
 
-    public void setTipo(int tipo) {
-        this.tipo = tipo;
+    public void setType(int type) {
+        this.type = type;
     }
 
-    public int getPrioridad() {
-        return prioridad;
+    public int getPriority() {
+        return priority;
     }
 
-    public void setPrioridad(int prioridad) {
-        this.prioridad = prioridad;
+    public void setPriority(int priority) {
+        this.priority = priority;
     }
 
 
 
-    public void iniciarEjecucion() {
+    public void initExecution() {
         if (this.robot != null) {
-            switch (this.tipo) {
-                case Action.DISPARAR: robot.setFire(parametro); break;
-                case Action.AVANZAR: robot.setAhead(parametro); break;
-                case Action.RETROCEDER: robot.setBack(parametro); break;
+            switch (this.type) {
+                case Action.SHOOT: robot.setFire(parameter); break;
+                case Action.AHEAD: robot.setAhead(parameter); break;
+                case Action.BACK: robot.setBack(parameter); break;
                 case Action.STOP: robot.setStop(); break;
-                case Action.GIRAR_CANON_DER: robot.setTurnGunRight(parametro); break;
-                case Action.GIRAR_CANON_IZQ: robot.setTurnGunLeft(parametro); break;
-                case Action.GIRAR_RADAR_DER: robot.setTurnRadarRight(parametro); break;
-                case Action.GIRAR_RADAR_IZQ: robot.setTurnRadarLeft(parametro); break;
-                case Action.GIRAR_TANQUE_DER: robot.setTurnRight(parametro); break;
-                case Action.GIRAR_TANQUE_IZQ: robot.setTurnLeft(parametro); break;
+                case Action.GUN_RIGHT: robot.setTurnGunRight(parameter); break;
+                case Action.GUN_LEFT: robot.setTurnGunLeft(parameter); break;
+                case Action.RADAR_RIGHT: robot.setTurnRadarRight(parameter); break;
+                case Action.RADAR_LEFT: robot.setTurnRadarLeft(parameter); break;
+                case Action.TANK_RIGHT: robot.setTurnRight(parameter); break;
+                case Action.TANK_LEFT: robot.setTurnLeft(parameter); break;
             }
         }
     }
@@ -78,20 +78,20 @@ public class Action {
     }
 
     public String toString(){
-        String etqTipo="";
-            switch (this.tipo) {
-                case Action.DISPARAR:etqTipo="Disparar"; break;
-                case Action.AVANZAR: etqTipo="Avanzar"; break;
-                case Action.RETROCEDER: etqTipo="Retroceder"; break;
-                case Action.STOP: etqTipo="Stop"; break;
-                case Action.GIRAR_CANON_DER: etqTipo="Girar cañón derecha"; break;
-                case Action.GIRAR_CANON_IZQ: etqTipo="Girar cañón izquierda"; break;
-                case Action.GIRAR_RADAR_DER: etqTipo="Girar radar derecha"; break;
-                case Action.GIRAR_RADAR_IZQ: etqTipo="Girar radar izquierda"; break;
-                case Action.GIRAR_TANQUE_DER: etqTipo="Girar tanque derecha"; break;
-                case Action.GIRAR_TANQUE_IZQ: etqTipo="Girar tanque izquierda"; break;
+        String strType="";
+            switch (this.type) {
+                case Action.SHOOT:strType="Shoot"; break;
+                case Action.AHEAD: strType="Going Ahead"; break;
+                case Action.BACK: strType="Going Back"; break;
+                case Action.STOP: strType="Stop"; break;
+                case Action.GUN_RIGHT: strType="Gun Right"; break;
+                case Action.GUN_LEFT: strType="Gun Left"; break;
+                case Action.RADAR_RIGHT: strType="Radar Right"; break;
+                case Action.RADAR_LEFT: strType="Radar Left"; break;
+                case Action.TANK_RIGHT: strType="Tank Right"; break;
+                case Action.TANK_LEFT: strType="Tank Left"; break;
             }
-	return "Accion[tipo:"+etqTipo+", param:"+parametro+", prioridad:"+prioridad+"]";
+	return "Action[type:"+strType+", parameter:"+parameter+", priority:"+priority+"]";
     }
 
 }
