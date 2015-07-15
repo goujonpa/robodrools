fd3ddb0ce20b35a29ed84d290de04a4b40b20746
goujonpa <goujonpa@gmail.com>
2015-07-15 19:37:07 +0200

    translation Helper

--- src/main/java/baguette/Helper.java
+++ src/main/java/baguette/Helper.java
@@ -2,92 +2,54 @@ package baguette;
 
 public class Helper {
 
-	/**
-	 * Calcula el angulo absolulo a partir de un angulo absoluto base y un angulo relativo (desplazamiento)
-	 * @param anguloBase
-	 * @param anguloRelativo
-	 * @return
-	 */
-    public static double anguloAbsoluto(double anguloBase, double anguloRelativo) {
-        double angulo = (anguloBase + anguloRelativo) % 360;
+	// calculate absolute angle from absolute angle y based and relative angle
+    public static double absoluteAngle(double baseAngle, double relativeAngle) {
+        double angle = (baseAngle + relativeAngle) % 360;
 
-        if (angulo < 0) {
-            angulo += 360;
+        if (angle < 0) {
+            angle += 360;
         }
 
-        return angulo;
+        return angle;
     }
 
-    /**
-     * Transforma un ángulo absoluto en un ángulo relativo a partir de un angulo base absoluto
-     * @param anguloBase
-     * @param anguloDestino
-     * @return
-     */
-    public static double anguloRelativo(double anguloBase, double anguloDestino) {
-        double angulo = (anguloDestino - anguloBase) % 360;
-        if (angulo > 180) {
-            angulo -= 360;
-        } else if (angulo < -180) {
-            angulo += 360;
+    // Transforms absolute angle into a relative angle, from an absolute base angle
+    public static double relativeAngle(double baseAngle, double destAngle) {
+        double angle = (destAngle - baseAngle) % 360;
+        if (angle > 180) {
+            angle -= 360;
+        } else if (angle < -180) {
+            angle += 360;
         }
 
-        return angulo;
+        return angle;
     }
 
-    /**
-     * Calcula la coordenada X de un punto a partir de una coordenada X base, un angulo y una distancia
-     * @param xBase
-     * @param anguloAbsoluto
-     * @param distancia
-     * @return
-     */
-    public static double calcularX(double xBase, double anguloAbsoluto, double distancia) {
-        double offsetX = (Math.sin(Math.toRadians(anguloAbsoluto)) * distancia);
+    // calculate a point's x from its base y, an angle and a distance
+    public static double calculateX(double xBase, double absoluteAngle, double distance) {
+        double offsetX = (Math.sin(Math.toRadians(absoluteAngle)) * distance);
         return xBase + offsetX;
     }
 
-    /**
-     * Calcula la coordenada Y de un punto a partir de una coordenada Y base, un angulo y una distancia
-     * @param yBase
-     * @param anguloAbsoluto
-     * @param distancia
-     * @return
-     */
-    public static double calcularY(double yBase, double anguloAbsoluto, double distancia) {
-        double offsetY = (Math.cos(Math.toRadians(anguloAbsoluto)) * distancia);
+    // calculate a point's y from its base x , an angle and a distance
+    public static double calcularY(double yBase, double absoluteAngle, double distance) {
+        double offsetY = (Math.cos(Math.toRadians(absoluteAngle)) * distance);
         return yBase + offsetY;
     }
 
-    /**
-     * Calcula el angulo absoluto entre dos pares de puntos (origen y destino)
-     * @param xOrigen
-     * @param yOrigen
-     * @param xDestino
-     * @param yDestino
-     * @return
-     */
-    public static double anguloAbsoluto(double xOrigen, double yOrigen, double xDestino, double yDestino) {
-        double offsetX = xDestino - xOrigen;
-        double offsetY = yDestino - yOrigen;
+    // calculate the absolute angle between two points (origin and destination)
+    public static double absoluteAngle(double xOrigin, double yOrigin, double xDestination, double yDestination) {
+        double offsetX = xDestination - xOrigin;
+        double offsetY = yDestination - yOrigin;
 
         return Math.toDegrees(Math.atan2(offsetX, offsetY));
     }
 
-    /**
-     * Calcula la distancia entre dos puntos
-     * @param xOrigen
-     * @param yOrigen
-     * @param xDestino
-     * @param yDestino
-     * @return
-     */
-    public static double distancia(double xOrigen, double yOrigen, double xDestino, double yDestino) {
-        double offsetX = xDestino - xOrigen;
-        double offsetY = yDestino - yOrigen;
+    // calculate the distance between two points
+    public static double distance(double xOrigin, double yOrigin, double xDestination, double yDestination) {
+        double offsetX = xDestination - xOrigin;
+        double offsetY = yDestination - yOrigin;
 
         return Math.sqrt(offsetX*offsetX + offsetY*offsetY);
-    }
-
-    
+    }    
 }
